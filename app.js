989bb3102919be04770ddadcea8d20bdf9cd728e
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2022-01-22 11:47:47 -0500

    added gitignore file to ignore node modules

--- app.js
+++ app.js
@@ -1,50 +1,31 @@
- const printprofileData = profileDataArr => {
-     //This ...
-    //  for (let i = 0; i < profileDataArr.length; i += 1){
-    //      console.log(profileDataArr[i]);
-    //  }
-    //  console.log('==============');
+ const { Console } = require('console');
+const fs = require('fs');
 
-    //  //Is the same as this...
-     profileDataArr.forEach((profileItem) => {
-         console.log(profileItem)
-     });
- }
+ const profileDataArgs = process.argv.slice(2);
 
-const animalArray = ['dog', 'cat','pig'];
+ const [name, github] = profileDataArgs;
 
-animalArray.push('cow');
-console.log(animalArray);
+ const generatePage = (name, github) => {
+     return `
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+       <meta charset="UTF-8>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <title>Portfolio Demo</title>
+       </head>
 
-const personObj = {
-    name: 'Lernantino',
-    age: 99
-};
-console.log(personObj);
+       <body>
+        <h1>${name}</h1>
+        <h2><a href="https:github.com/${github}">Github</a></h2>
+       </body>
+       </html> 
+       `;   
+ };
 
-personObj.age = 100;
-personObj.occupation = 'Developer';
- console.log(personObj);
-
-
-let message = 'Hello Node!';
-message = 'Hello ES6!';
-
-let sum = 5 + 3;
-sum += 1;
-
-console.log(message,sum);
-
-// const printProfileData = (profileDataArr) => {
-//     console.log(profileDataArr);
-//   };
-  
-//   printProfileData(profileDataArgs);
-
-  const addNums = (numOne, numTwo) => numOne + numTwo;
-
-//   const sum = addNums =(5,3);
-
-//   console.log(numOne, numTwo);
-
-//   return numOne + numTwo;
+ fs.writeFile('./index.html', generatePage(name, github), err => {
+     if (err) throw new Error(err);
+       
+        console.log('')
+    });
