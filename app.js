b6dae327e82044347d00416055683f8bcc1cef38
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2022-01-23 10:10:45 -0500

    validation property added to 5 prompts

--- app.js
+++ app.js
@@ -30,13 +30,111 @@ const inquirer = require('inquirer');
        
         console.log('')
     });
+    const promptUser = () => {
+        return inquirer.prompt([
+                {
+                    type:'input',
+                    name: 'name',
+                    message:'What is your name? (Required)',
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter your name!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name:'github',
+                    message:'Enter your GitHub Username (Required)',
+                    validate:(githubNameInput) => {
+                        if (githubNameInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter your Github Username!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type:'input',
+                    name:'about',
+                    message:'Provide some information about yourself:',
+                }
+            ])  
+    };
 
-    inquirer
-    .prompt([
-        {
-            type:'input',
-            name: 'name',
-            message:'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
+    const promptProject = () => {
+        console.log(`
+        =================
+        Add a New Project
+        =================
+        `);
+        return inquirer.prompt([
+            {
+                type:'input',
+                name: 'name',
+                message: 'What is the name of your project? (Required)',
+                validate: ProjectNameInput => {
+                    if (ProjectNameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your Project Name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type:'input',
+                name:'description',
+                message:'Provide a description of the project (Required)',
+                validate: ProjectDescInput => {
+                    if (ProjectDescInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a Project Description!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type:'checkbox',
+                name:'languages',
+                message:'What did you build this project with? (Check all that apply)',
+                choices:['JavaScript', 'HTML','CSS','ES6','jQuery','Bootstrap','Node']
+            },
+            {
+                type: 'input',
+                name:'link',
+                message: 'Enter the Github link to your project.(Required)',
+                validate: GithubLinkInput => {
+                    if (GithubLinkInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your project\'\s Github link!');
+                        return false;
+                    }
+            }
+        },
+            {
+                type: 'confirm',
+                name:'feature',
+                message:'Would you like to feature this project?',
+                default: false
+            },
+            
+            {
+                type:'confirm',
+                name:'confirmAddProject',
+                message:'Would you like to enter another project?',
+                default: false
+            }
+        ])
+    };
+    promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
+    
