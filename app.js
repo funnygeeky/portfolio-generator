0e5488e5e3f4255942be93d5973bf40d0b44b2d4
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2022-01-26 11:19:49 -0500

    dist directory added and updated code to put html there pluss css stylesheet

--- app.js
+++ app.js
@@ -129,14 +129,38 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    // fs.writeFile('./dist/index.html');
+    return generatePage(portfolioData);
+     //const pageHTML = generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+    fs.writeFile('./dist/index.html');
     fs.writeFile('./dist/index.html', pageHTML, err => {
         if (err) throw new Error(err);
-  
-        console.log('Page created! Check out index.html in this directory to see it!');
-      });
+          console.log(err);
+          return;
     });
+        console.log('Page created! Check out index.html in this directory to see it!');
+      
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('Style sheet copied successfully!');
+        });
+      
+    
 
   
